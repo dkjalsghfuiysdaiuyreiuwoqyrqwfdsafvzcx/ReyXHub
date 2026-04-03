a88426d0479c274ca2c9a56340899f525af24b88
reynaldo baja Jr <rb@awork.dk>
2026-04-03 15:48:34 +0800

    generateapikey

--- lib/actions/api-key.ts
+++ lib/actions/api-key.ts
@@ -1,6 +1,13 @@
 import { redirect } from "next/navigation"
 import { getSession, logoutEmail } from "./auth-actions"
 import prisma from "../prisma"
+import crypto from "crypto";
+
+type GenerateApiKeyParams = {
+  userId: string;
+  maxRequests?: number;
+  expiresAt?: Date | null;
+};
 
 
 export async function getUserApiKey(userId: string) {
@@ -11,7 +18,6 @@ export async function getUserApiKey(userId: string) {
         redirect("/login")
     }
 
-
     const userApiKey = await prisma.apiKey.findMany({
         where: {
             userId: userId
@@ -19,4 +25,24 @@ export async function getUserApiKey(userId: string) {
     })
 
     return userApiKey
+}
+
+export async function generateApiKey({
+  userId,
+  maxRequests = 1000,
+  expiresAt = null,
+}: GenerateApiKeyParams) {
+  // 🔑 generate secure key
+  const key = crypto.randomBytes(32).toString("hex");
+
+  const apiKey = await prisma.apiKey.create({
+    data: {
+      key,
+      maxRequests,
+      expiresAt,
+      userId,
+    },
+  });
+
+  return apiKey;
 }
